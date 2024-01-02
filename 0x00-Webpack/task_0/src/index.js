f28ea70a6396e455f7306a87d45dfe9307f312a1
amanuelgthn <amanuelgthn@gmail.com>
2024-01-02 17:11:25 +0100

    update

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,11 +1,5 @@
 import $ from "jquery";
 
-function addparagraph() {
-    const paragraph1 = $("<p>).text('Holberton Dashboard').</p>");
-    const paragraph2 = $("<p>).text('Dashboard data for the students').</p>");
-    const paragraph3 = $("<p>).text('Copyrigh - Holberton School').</p>");
-
-    $('body').append(paragraph1, paragraph2, paragraph3);
-}
-
-addparagraph();
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
