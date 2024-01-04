e6690ae5aadacff7d879867cc0af1945dbbeb0ff
amanuelgthn <amanuelgthn@gmail.com>
2024-01-04 17:57:33 +0100

    update

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,29 +1,34 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/dashboard_main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                loader: "css-loader",
-                options: {
-                    url: true,
-                },
-            },
-            {
-                test: /\.(png|jpe?g|gif)s/i,
-                use: [
-                    {
-                        loader: "file-loader",
-                    },
-                ],
-            },
-        ],
-    },
+	entry: './js/dashboard_main.js',
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				],
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true,
+							disable: true,
+						},
+					},
+				],
+			}
+		]
+	}
 };
