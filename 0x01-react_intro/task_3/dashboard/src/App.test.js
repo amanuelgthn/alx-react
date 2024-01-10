0c9d5e2a8613e501e2952edc38b0966a56bf0de7
amanuelgthn <amanuelgthn@gmail.com>
2024-01-10 12:35:27 +0100

    Add test Notifications.test.js

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+	it('Test App if crushes', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.exists()).toBe(true);
+	})
+
+	it('Test App renders a <div> with class "App-header"', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('.App-header').length).toBe(1);
+	})
+
+	it('Test App renders a <div> with class "App-body"', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('.App-body').length).toBe(1);
+	})
+
+	it('Tests App renders a <div> with class "App-footer"', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('.App-footer').length).toBe(1);
+	})
 });
