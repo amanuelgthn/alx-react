3077050f82c51730aff8978ce9e1d7ceb2deb7b1
amanuelgthn <amanuelgthn@gmail.com>
2024-01-10 11:55:11 +0100

    install and configure enzyme for react

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'; // Replace xx with your React version
+
+configure({ adapter: new Adapter() });
